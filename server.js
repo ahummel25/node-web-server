ed769d870df3403573c839e051a9c7445e55ac55
Hummela <andrew.hummel@wwt.com>
2017-11-21 19:18:17 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const hbs = require ('hbs')
+const fs = require ('fs')
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+   var now = new Date().toString()
+   var log = `${now}: ${req.method} ${req.url}`
+   fs.appendFile('server.log', log + '\n', (err) => {
+       if (err) {
+           console.log('Unable to append to log file.')
+       }
+   })
+   next()
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'Welcome!'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'Unable to fulfill this request'
+    })
+})
+
+app.listen(8000, () => {
+    console.log('Server started')
+})
+
